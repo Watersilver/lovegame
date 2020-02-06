30887b35453f3a05755b6b4de9493c0e11a5bc09
ez <cydisalt@gmail.com>
2020-02-06 20:19:10 +0200

    created w097x101 and w098x101

--- Rooms/w099x101.lua
+++ Rooms/w099x101.lua
@@ -19,7 +19,13 @@ room.rightTrans = {
     xmod = 0, ymod = 0
   }
 }
-room.leftTrans = {}
+room.leftTrans = {
+  {
+    roomTarget = "Rooms/w098x101.lua",
+    yupper = 0, ylower = 520,
+    xmod = 0, ymod = 0
+  }
+}
 room.upTrans = {
   {
     roomTarget = "Rooms/w099x100.lua",
