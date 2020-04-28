c274fe8bc9a0aa5c70de5ab42f96c13179203fcc
ez <cydisalt@gmail.com>
2020-04-28 21:57:09 +0300

    added 90x96, 90x97, 90x98, 91x96, 91x97, 91x98, 92x97, 92x98, 93x96, 95x103

--- Rooms/w093x098.lua
+++ Rooms/w093x098.lua
@@ -19,7 +19,13 @@ room.rightTrans = {
     xmod = 0, ymod = 0
   }
 }
-room.leftTrans = {}
+room.leftTrans = {
+  {
+    roomTarget = "Rooms/w092x098.lua",
+    yupper = 0, ylower = 520,
+    xmod = 0, ymod = 0
+  }
+}
 room.upTrans = {
   {
     roomTarget = "Rooms/w093x097.lua",
