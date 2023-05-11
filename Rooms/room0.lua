baecdfc14cd34d182d48464d4a1871be0af00043
Ermis <cydisalt@gmail.com>
2023-05-11 23:53:07 +0300

    starting room overhaul

--- Rooms/room0.lua
+++ Rooms/room0.lua
@@ -1,18 +1,16 @@
-local rm = require("RoomBuilding.room_manager")
-local sh = require "scaling_handler"
-local trans = require "transitions"
-local game = require "game"
-
 -- Special starting room that doesn't quite follow room rules
 -- Used to create the persistent player object
 -- (Yes I could have created it a different way no real reason for this room)
+-- 11-5-2023 update: I am a fucking asshole. Why would I do this? Any of this? I suck. Will attempt to fix.
+-- 11-5-2023 update: radically changed this to make as sane as possible. Let it stand as a testament to my suckiness.
+
 local room = {}
+room.newType = true
 
 room.width = 800
 room.height = 450
 
-room.dontbuildedgetiles = true
-
+-- 11-5-2023 update: what the fuck was I thinking.
 mainCamera:setWorld(0, 0, room.width, room.height)
 
 room.downTrans = {}
@@ -22,26 +20,10 @@ room.upTrans = {}
 
 room.game_scale = 2
 
-room.room_parts = {}
+room.gameObjects = {}
 
-----------Start of arrays of geography of parts of room----------
-local room_part = {blueprint = 'PlayaTest'}
--- room_part.x_that_I_start = session.save.playerX or 300
--- room_part.y_that_I_start = session.save.playerY or 200
-room_part.x_that_I_start = session.save.playerX or 55
-room_part.y_that_I_start = session.save.playerY or 55
+room.manuallyPlacedObjects = {
+  {x = session.save.playerX or 55, y = session.save.playerY or 55, blueprint = "PlayaTest"},
+}
 
-table.insert(room.room_parts, room_part)
----
-----------End of arrays of geography of parts of room----------
-
-
-rm.build_room(room)
-
--- -- Make sure there's no camera weirdness, even if I start in a tiny room
--- sh.calculate_total_scale{game_scale=11}
--- return assert(love.filesystem.load("Rooms/room1.lua"))()
-return game.change_room(session.save.room or
--- "Rooms/newTilesTestRoom.lua"
-"Rooms/LakeVillage/startingHouse.lua"
-)
+return room
