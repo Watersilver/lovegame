8b54924b141c20b0199799518a70ce40e1a78091
Ermis <cydisalt@gmail.com>
2024-02-04 05:28:10 +0200

    enable vsync

--- conf.lua
+++ conf.lua
@@ -1,6 +1,6 @@
 function love.conf(t)
   t.identity = "Mage"
-  t.window.vsync = false
+  t.window.vsync = true
   t.window.highdpi = false
   t.window.fullscreentype = "desktop"
   t.window.fullscreen = false
