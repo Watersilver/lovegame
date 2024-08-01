4696c03f25e5aaa77c7648d98e79721ac5586fcd
Ermis <cydisalt@gmail.com>
2024-08-01 19:56:31 +0300

    crap

--- Rooms/room0.lua
+++ Rooms/room0.lua
@@ -3,6 +3,8 @@
 -- (Yes I could have created it a different way no real reason for this room)
 -- 11-5-2023 update: I am a fucking asshole. Why would I do this? Any of this? I suck. Will attempt to fix.
 -- 11-5-2023 update: radically changed this to make as sane as possible. Let it stand as a testament to my suckiness.
+-- 6-27-2024 update: What? What's going on here? How do I detect where the onLoad room is??? Why why why why
+-- 6-27-2024 update: Found it. Its in the PlayaTest initialize function. Fuck.
 
 local room = {}
 room.newType = true
